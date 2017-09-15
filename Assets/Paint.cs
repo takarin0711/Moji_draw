d31d574c4fdea89a88e32cde90e8795ebd024a95
猪膝孝之 <takayuki_0711sosdx@yahoo.co.jp>
2017-09-15 20:04:53 +0900

    initial commit

---
+++ Assets/Paint.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Es.InkPainter;
+using UnityEngine.VR.WSA.Input;
+
+public class Paint : MonoBehaviour
+{
+
+    [SerializeField]
+    private Brush brush;
+
+    private bool pressflag = false;
+
+    private void Start()
+    {
+        InteractionManager.SourceUpdated += InteractionManager_SourceUpdated;
+        InteractionManager.SourcePressed += InteractionManager_SourcePressed;
+        InteractionManager.SourceReleased += InteractionManager_SourceReleased;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void InteractionManager_SourceUpdated(InteractionSourceState state)
+    {
+        if (pressflag == true)
+        {
+            Vector3 v;
+            state.properties.location.TryGetPosition(out v);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(v, Camera.main.transform.forward, out hitInfo))
+            {
+                var paintObject = hitInfo.transform.GetComponent<InkCanvas>();
+                if (paintObject != null) paintObject.Paint(brush, hitInfo);
+            }
+        }
+
+    }
+
+    void InteractionManager_SourcePressed(InteractionSourceState state)
+    {
+        pressflag = true;
+    }
+
+    void InteractionManager_SourceReleased(InteractionSourceState state)
+    {
+        pressflag = false;
+    }
+}
